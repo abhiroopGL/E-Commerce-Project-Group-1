7f388b8fb12882000d3e441f8b0b89b646912e45
abhiroopGL <abhiroop.panchal@globallogic.com>
2022-07-25 17:37:27 +0530

    Changes made by Abhiroop || 5:28 pm

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Display from './Display';
 import Nav from './Nav';
 import ProductList from './ProductList';
+import ProductContext from './contexts/productContext';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Display/>
+      <ProductContext/>
       <ProductList/>
     </div>
   );
