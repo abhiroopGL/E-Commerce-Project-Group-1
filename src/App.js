3681225391260017f23df98d0826502f8d2b59ef
Pranshu <pranshup23@gmail.com>
2022-07-25 17:37:51 +0530

    adding header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Display from './Components/Display';
-import Nav from './Components/Nav';
+import Header from './Components/Header';
 import ProductList from './Components/ProductList';
 import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Header/>
       <Display/>
       <ProductList/>
       <Footer />
