d5d931a2dbf33bb26b58e07ee79250176799ef8b
FalakNaaz <falucknaz@gmail.com>
2022-07-26 17:58:06 +0530

    side bar change

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Offcanvas } from 'react-bootstrap';
 import {ProductContext} from '../contexts/productContext'
+import {Link} from "react-router-dom";
+import '../App.css'
 
 function Sidebar() {
     const closeSidebar = () => setShowSidebar(false);
@@ -12,10 +14,16 @@ function Sidebar() {
             <Container className='p-4 bg-dark' >
                 <Offcanvas show={showSidebar} onHide={closeSidebar} className = "bg-dark text-white">
                     <Offcanvas.Header closeButton >
-                        <Offcanvas.Title>Sidebar Title</Offcanvas.Title>
+                        <Offcanvas.Title style={{fontSize: "30px"}} >MyExcelShop</Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
-                        Some dummy text, we can have any text or element at at this place.
+                        <ul style={{listStyle: "none", fontSize: "30px"}}  >
+                        <li > <Link to = "/" className = "link">Home</Link></li>
+                            <li><Link to = "/" className = "link">About</Link></li>
+                            <li><Link to = "/" className = "link">Contact Us</Link></li>
+                            <li><Link to = "/login" className = "link">Log Out</Link></li>
+
+                        </ul>
                     </Offcanvas.Body>
                 </Offcanvas>
             </Container>
