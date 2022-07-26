28cb9631678fd71415155ac4d6586d9de3f8fd44
FalakNaaz <falucknaz@gmail.com>
2022-07-26 16:57:50 +0530

    "commit"

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 import ForgotPassword from './Components/ForgotPassword';
-<<<<<<< HEAD
 import ProductDetails from './Components/ProductDetails';
-=======
 import Dashboard from './Components/Dashboard';
->>>>>>> 005bb3b066ade55bc8737479738cae68e1b3538e
 
 function App() {
   return (
