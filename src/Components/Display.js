4688c355b32858c6157474338aea33c7fc867b86
Pranshu <pranshup23@gmail.com>
2022-07-26 11:25:38 +0530

    addding images

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -5,7 +5,6 @@ import Carousel from 'react-bootstrap/Carousel';
 function Display() {
   return (
     <Carousel>
-<<<<<<< HEAD
     <Carousel.Item>
       <img id='carouselImg'
         className="d-block w-100"
@@ -39,46 +38,6 @@ function Display() {
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
-=======
-      <Carousel.Item>
-        <img id='carouselImg'
-          className="d-block w-100"
-          src="https://www.w3schools.com/images/w3schools_green.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item >
-        <img id='carouselImg'
-          className="d-block w-100"
-          src="https://www.w3schools.com/images/w3schools_green.jpg"
-          alt="First slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img id='carouselImg'
-          className="d-block w-100"
-          src="https://www.w3schools.com/images/w3schools_green.jpg"
-          alt="First slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
->>>>>>> 0a52f26b722b78b5bfcf2eeeb7977839f2e56412
   )
 }
 export default Display;
