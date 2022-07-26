b89a77fcec2ec1e83ab62c11e47a426cdb75cf4b
abhiroopGL <abhiroop.panchal@globallogic.com>
2022-07-26 12:50:43 +0530

    12:50

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -12,7 +12,7 @@ function ProductList() {
                 productList.map((item,index) => (
                     <Card key={index} style={{ width: '40vh', diplay: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', marginTop: '3vh' }}>
                         <Card.Img variant="top" src={item.image} style={{ height: '35vh', width: '28vh' }} />
-                        <Card.Body>
+                        <Card.Body style={{diplay: 'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center'}}>
                             <Card.Title>{item.title}</Card.Title>
                             <Button variant="primary" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>Add to Cart</Button>
                         </Card.Body>
